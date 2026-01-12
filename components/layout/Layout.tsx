ae940f3846be9a3cae994351f0c8b3fd0b09b777
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2026-01-12 18:32:19 +0100

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,8 +1,8 @@
 // components/Layout.tsx
 
 import React from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
